9e64668a5dd0d0e4e2a8f1658a31c46bcc60d6a8
aryyudha.project <aryyudhanugroho03@gmail.com>
2023-06-13 19:09:01 +0700

    Membuat Class Mahasiswa dan Mahasiswaberaksi

---
+++ src/Universitas/Mahasiswa.java
@@ -0,0 +1,26 @@
+
+package Universitas;
+
+
+public class Mahasiswa {  
+package universitas;
+public class Mahasiswa {
+    private String npm, nama, prodi;
+    
+    public Mahasiswa(String npm, String nama, String prodi){
+        this.npm = npm;
+        this.nama = nama;
+        this.prodi = prodi;
+    }
+    public void setNPM(String npm) {
+        this.npm = npm;
+    }
+    
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+}
---
+++ src/Universitas/Mahasiswaberaksi.java
@@ -0,0 +1,16 @@
+
+package Universitas;
+
+
+public class Mahasiswaberaksi {
+    public static void main(String[] args) {
+        public static void main(String[] args) {
+        //Membuat objek
+        Mahasiswa yudha = new mahasiwa (npm,nama,prodi);
+        System.out.println(" Nama NPM:";
+        System.out.println(" Nama Mahasiswa:"
+        System.out.println(" Prodi ));"
+            
+              
+    }
+}
